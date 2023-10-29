764b2447bcbe98db54220ee7d1419ec4df969e6b
UserHab <vitalibembel1.21@gmail.com>
2023-10-29 23:48:05 +0300

    1

---
+++ hello/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from hello import views
+
+urlpatterns = [
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("hello/<name>", views.hello_there, name="hello_there"),
+    path("", views.home, name="home"),
+]
