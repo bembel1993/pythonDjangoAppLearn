3fb4b51525470cabe091246c6ebdc50548f8d13c
Vitali <bembel1.20@gmail.com>
2024-02-01 00:25:57 +0300

    29

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
+    path("members/", views.members, name="members"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("hello/<name>", views.hello_there, name="hello_there"),
--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,11 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Member
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 def home(request):
     return HttpResponse("Hello, Django!")
@@ -21,7 +26,25 @@ def home(request):
     return render(request, "hello/home.html")
 
 def about(request):
+    driver = webdriver.Chrome()
+    driver.get("https://autodocxsystem.by/")
+    assert "Super Inform Soft" in driver.title
+    logform = driver.find_element(By.ID, "LogForm")
+    inputname = driver.find_element(By.ID, "name")
+    submitbtn = driver.find_element(By.ID, "submit")
+    inputname.send_keys("Chief Creator")
+    submitbtn.click()
+
+    driver.close()
     return render(request, "hello/about.html")
 
 def contact(request):
     return render(request, "hello/contact.html")
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template("hello/all_members.html")
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
